6b3f6530ecdbcf2c2e3f4b72a59ece392f7f4c0b
Martin Budden <mjbudden@gmail.com>
2024-08-27 08:33:33 +0100

    Added motor, iec, fan and spoolholder cutouts to RightFace.py.

--- cadquery/RightFace.py
+++ cadquery/RightFace.py
@@ -4,6 +4,9 @@ from LeftFace import leftFaceHoles
 from typing import (TypeVar)
 T = TypeVar("T", bound="Workplane")
 
+from fans import fan30x10, fan_bore, fan_hole_pitch
+from stepper_motors import NEMA17_40, NEMA_boss_radius, NEMA_hole_pitch
+
 from constants import cncCuttingRadius, fittingTolerance, lsrKerf, lsrCuttingRadius
 from constants import backPlateThickness, sizeZ
 from constants import M3_clearance_radius
@@ -21,12 +24,45 @@ def rightFace(
 
     result = leftFaceHoles(self, sizeX, sizeY, kerf, grill=False)
 
-    #range(start, end, step)
+    fan = fan30x10
+    result = result \
+        .moveTo(68 - sizeX/2, 25 - sizeY/2) \
+        .circle(fan_bore(fan)/2 - 0.5) \
+        .rect(2*fan_hole_pitch(fan), 2*fan_hole_pitch(fan), forConstruction=True) \
+        .vertices() \
+        .circle(M3_clearance_radius - kerf/2)
+
+    motor = NEMA17_40
+    result = result \
+        .moveTo(sizeX/2 - 75, sizeY/2 - 73) \
+        .circle(NEMA_boss_radius(motor) + 0.25) \
+        .rect(NEMA_hole_pitch(motor), NEMA_hole_pitch(motor), forConstruction=True) \
+        .vertices() \
+        .circle(M3_clearance_radius - kerf/2)
+
+    # spool holder
+    result = result \
+        .pushPoints([(x - sizeX/2, sizeY/2 - 80) for x in [8, 45]]) \
+        .circle(M3_clearance_radius - kerf/2)
+
+    # ICE
+    iecPosition = (175.5 - sizeX/2, 33 - sizeY/2)
+    result = result \
+        .pushPoints([(iecPosition[0], iecPosition[1] + y) for y in [-20, 20]]) \
+        .circle(M3_clearance_radius - kerf/2)
+    result = result.extrude(sizeZ) \
+        .moveTo(iecPosition[0], iecPosition[1]) \
+        .sketch().rect(48, 30) \
+        .vertices() \
+        .fillet(5) \
+        .finalize() \
+        .cutThruAll()
+
     leftDogbones = [(-sizeX/2, i - sizeY/2) for i in range(30, 210 + 1, 40)]
     rightDogbones = [(sizeX/2, i - sizeY/2) for i in range(30, 210 + 1, 40)]
     topDogbones = [(i - sizeX/2, sizeY/2) for i in range(30, 190 + 1, 40)]
 
-    result = result.extrude(sizeZ) \
+    result = result \
         .pushPoints(rightDogbones) \
         .dogboneT(20, 6, cuttingRadius, 90, dogboneTolerance).cutThruAll() \
         .pushPoints(leftDogbones) \
---
+++ cadquery/fans.py
@@ -0,0 +1,7 @@
+fan30x10 = [30,  10, 27, 12]
+fan40x11 = [40,  11, 37, 16]
+
+def fan_width(type):          return type[0]    #! Width of square
+def fan_depth(type):          return type[1]    #! Depth of fan
+def fan_bore(type):           return type[2]    #! Diameter of the hole for the blades
+def fan_hole_pitch(type):     return type[3]    #! Screw hole pitch
