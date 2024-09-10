0a84350e8ea4caa5d3b4616290fcab257ec937de
Martin Budden <mjbudden@gmail.com>
2024-09-10 15:57:24 +0100

    Whitespace tidy.

--- cadquery/fans.py
+++ cadquery/fans.py
@@ -1,5 +1,5 @@
-fan30x10 = [30,  10, 27, 12]
-fan40x11 = [40,  11, 37, 16]
+fan30x10 = [30, 10, 27, 12]
+fan40x11 = [40, 11, 37, 16]
 
 def fan_width(type):          return type[0]    #! Width of square
 def fan_depth(type):          return type[1]    #! Depth of fan
